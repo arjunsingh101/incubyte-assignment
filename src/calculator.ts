ea2f4aa0f8675b77dc4e599542dd6a3c8a02caee
arjun-hbits <arjuns@hbits.co>
2024-11-13 00:03:59 +0530

    feat: text cases for custom  delimiter

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,6 +1,13 @@
 export const add = (numbers: string): number => {
   if (!numbers) return 0;
 
-  const nums = numbers.split(/,|\n/).map(Number);
+  let delimiter = /,|\n/;
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(parts[0].slice(2));
+    numbers = parts[1];
+  }
+
+  const nums = numbers.split(delimiter).map(Number);
   return nums.reduce((acc, num) => acc + num, 0);
 };
